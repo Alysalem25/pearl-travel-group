f2cd8ab13911aff01cd159610842163fd960b914
Alysalem25 <alysalem25t@gmail.com>
2026-03-19 00:06:08 +0200

    change users upload , try category no change

--- server/middlewares/uploadUser.js
+++ server/middlewares/uploadUser.js
@@ -19,13 +19,61 @@
 // module.exports = uploadUser;
 
 
-const multer = require("multer");
+// const multer = require("multer");
+// const path = require("path");
+// const crypto = require("crypto");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "/uploads/users");
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = path.extname(file.originalname);
+//     const uniqueName =
+//       Date.now() + "-" + crypto.randomBytes(6).toString("hex") + ext;
+//     cb(null, uniqueName);
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   const allowed = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
+//   if (allowed.includes(file.mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only images are allowed"), false);
+//   }
+// };
+
+// const uploadUser = multer({
+//   storage,
+//   fileFilter,
+//   limits: { fileSize: 10 * 1024 * 1024 },
+// });
+
+// module.exports = uploadUser;
+
+
+
+
+
+
+
+
+const fs = require("fs");
 const path = require("path");
+const multer = require("multer");
 const crypto = require("crypto");
+
+const uploadPath = path.join(__dirname, "../uploads/users");
+
+// ensure folder exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "/uploads/users");
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
@@ -44,10 +92,8 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const uploadUser = multer({
+module.exports = multer({
   storage,
   fileFilter,
   limits: { fileSize: 10 * 1024 * 1024 },
 });
-
-module.exports = uploadUser;
